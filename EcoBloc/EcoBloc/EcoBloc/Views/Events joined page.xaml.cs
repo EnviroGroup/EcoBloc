c0ba07d56d70c08d74db97eb1e6cd8350c4d9a07
spiritsage <62544132+spiritsage@users.noreply.github.com>
2021-02-09 17:40:14 +0200

    It all begins now :)

---
+++ EcoBloc/EcoBloc/EcoBloc/Views/Events joined page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Eco_Bloc_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Events_joined_page : ContentPage
+    {
+        public Events_joined_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
