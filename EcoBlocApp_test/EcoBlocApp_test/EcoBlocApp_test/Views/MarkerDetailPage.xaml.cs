5a7df296eb7fe0c966bcec231f0a551c63b1a1d0
spiritsage <62544132+spiritsage@users.noreply.github.com>
2021-02-10 20:53:21 +0200

    Add the temp pages

---
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Views/MarkerDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EcoBlocApp_test.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Marker_detail_page : ContentPage
+    {
+        public Marker_detail_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
