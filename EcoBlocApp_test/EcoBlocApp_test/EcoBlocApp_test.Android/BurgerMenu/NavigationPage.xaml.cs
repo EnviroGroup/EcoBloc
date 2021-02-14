8a5e1efcb08673a038105a23621f4c000f2abb80
Euan Heradien <euanheradien@gmail.com>
2021-02-14 23:47:55 +0200

    Am getting somewhere

---
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test.Android/BurgerMenu/NavigationPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Web.UI;
+
+namespace EcoBlocApp_test.Droid.BurgerMenu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPage : ContentPage
+    {
+        private NavigationPage(MainPage mainpage)
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        public MainPage
+        MainPage = new MasterDetailPage.FocusRequestArgs()
+        {
+            master = new MasterPage()
+            { Title = "Main Page" },
+    Detail = new NavigationPage(new MainPage.MasterPage())
+};
+    }
+}
--- EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test.Android/BurgerMenu/XamlCompilationAttribute.cs
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test.Android/BurgerMenu/XamlCompilationAttribute.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using Xamarin.Forms.Xaml;
 
 namespace EcoBlocApp_test.Droid.BurgerMenu
 {
     internal class XamlCompilationAttribute : Attribute
     {
+        private XamlCompilationOptions compile;
+
+        public XamlCompilationAttribute(XamlCompilationOptions compile)
+        {
+            this.compile = compile;
+        }
     }
 }
