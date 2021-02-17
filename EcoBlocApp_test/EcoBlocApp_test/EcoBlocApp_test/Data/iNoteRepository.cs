67930fdf3558f7fa51ae125b02e35a2a739822be
tumi-m <tumeloxmalebo@gmail.com>
2021-02-17 17:32:59 +0200

    SQL changes
    SQL changes

---
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Data/iNoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using XamNote.Models;
+using Xamarin.forms;
+
+namespace EcoBlocApp_test.Data
+{
+    public interface INoteRepository
+    {
+        Task Initialize();
+        Task<List<Note>> GetNotes();
+        Task AddOrUpdateNote(Note note);
+    }
+}
