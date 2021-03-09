0a15a43d676d5410c5ee819be64b4374f11c7325
Euanher <70320894+Euanher@users.noreply.github.com>
2021-03-09 02:44:03 +0200

    euan

---
+++ App1/App1/App1/Services/CameraPreview.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Services
+{
+    class public CameraPreview : View
+    {
+        public static readonly BindableProperty CameraProperty = BindableProperty.Create(propertyName: "Camera", returnType: typeof(CameraOptions), declaringType: typeof(CameraPreview), defaultValue: CameraOptions.Rear);
+        public CameraOptions Camera
+        {
+            get
+            {
+                return (CameraOptions)GetValue(CameraProperty);
+            }
+            set
+            {
+                SetValue(CameraProperty, value);
+            }
+        }
+    }
+public enum CameraOptions
+{
+    Rear,
+    Front
+}
+{
+    public enum CameraOptions()
+
+    }
+}
