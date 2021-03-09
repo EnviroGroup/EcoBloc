48bbd30acfbfd471897bf357f46e14e60bc9e80a
Euanher <70320894+Euanher@users.noreply.github.com>
2021-03-09 04:01:19 +0200

    Euan

--- App1/App1/App1/Views/LoginPage.xaml.cs
+++ App1/App1/App1/Views/LoginPage.xaml.cs
@@ -17,5 +17,21 @@ namespace App1.Views
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
         }
+        private async Task<ImageAnalysis> UploadAndAnalyzeImageAsync(string imageFilePath)
+{
+    using (var client = new ComputerVisionClient(Credentials) { Endpoint = Endpoint })
+    {
+        Log("ComputerVisionClient is created");
+
+        using (Stream imageFileStream = File.OpenRead(imageFilePath))
+        {
+            
+            Log("Calling ComputerVisionClient.AnalyzeImageInStreamAsync()...");
+            VisualFeatureTypes[] visualFeatures = GetSelectedVisualFeatures();
+            string language = (_language.SelectedItem as RecognizeLanguage).ShortCode;
+            ImageAnalysis analysisResult = await client.AnalyzeImageInStreamAsync(imageFileStream, visualFeatures, null, language);
+            return analysisResult;
+        }
+    }
     }
 }
