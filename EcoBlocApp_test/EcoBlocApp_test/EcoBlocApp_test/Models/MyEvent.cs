6bb9279124361a749dcbd235fc2c6c6ec682b21f
Enan Heradien <eheradien@my365-uwc.ac.za>
2021-03-11 17:06:12 +0200

    EuanEuancommit half

--- EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Models/MyEvent.cs
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Models/MyEvent.cs
@@ -7,8 +7,36 @@ using Xamarin.Forms.Maps;
 
 namespace EcoBlocApp_test.Models
 {
-    public class MyEvent
-    {
+    public class MyEvent : SignUpPageViewModel
     
+  
+        {
+        public class MyEventrRepository
+        {
+            private List<MyEvent> _myEvent;
+
+            public int Participant
+            { get; set; }
+            public myEventRepository()
+            {
+                _myEvent = new List<MyEvent>
+            {
+                new MyEvent(){ MyEventID = 1, Discription ="Dana Birkby", Participant =""},
+                new MyEvent(){ MyEventID = 2, Discription ="Adriana Giorgi", Participant =""},
+                new MyEvent(){ MyEventID = 3, Discription ="Wei Yu", Participant =""}
+            };
+            }
+
+            public List<MyEvent> GetMyEvent()
+            {
+                return _myEvent;
+            }
+
+            public void UpdateMyEvent(MyEvent SelectedMyEvent)
+            {
+                MyEvent myEventToChange = _myEvent.Single(c => c.MyEventID == SelectedMyEvent.MyEventID);
+                myEventToChange = SelectedMyEvent;
+            }
+        }
     }
 }
