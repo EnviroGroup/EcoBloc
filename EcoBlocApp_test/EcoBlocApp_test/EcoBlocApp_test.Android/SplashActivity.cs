7d97a79941b9eb75ff3c4bd09da4ae40d4100f55
Mickyle Otto <62544132+OttoCodes@users.noreply.github.com>
2021-03-15 15:46:59 +0200

    Mickyle - LocateDumpsiteMap added

--- EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test.Android/SplashActivity.cs
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test.Android/SplashActivity.cs
@@ -34,11 +34,11 @@ namespace EcoBlocApp_test.Droid
         }
 
         // Simulates background work that happens behind the splash screen
-        async void SimulateStartup()
+         void SimulateStartup()
         {
-            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
-            await Task.Delay(1000); // Simulate a bit of startup work.
-            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+           // Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            //await Task.Delay(200); // Simulate a bit of startup work.
+            //Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
     }
