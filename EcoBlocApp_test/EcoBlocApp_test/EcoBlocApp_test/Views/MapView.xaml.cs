32d7ff16a20f1cdf9df0c2774884854e17d27824
joshuapetersen1@outlook.com <joshuapetersen1@outlook.com>
2021-03-17 12:43:35 +0200

    joshua- floating button, login page update

--- EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Views/MapView.xaml.cs
+++ EcoBlocApp_test/EcoBlocApp_test/EcoBlocApp_test/Views/MapView.xaml.cs
@@ -15,26 +15,104 @@ namespace EcoBlocApp_test.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapView : ContentPage
     {
-       
+
         GeolocateUser userLocation;
         double[] geoLocate;
-      
+
 
         public MapView()
         {
-          
+
 
             InitializeComponent();
 
-            
-            
+
+
 
         }
+        private bool isOpen = false;
+        private async void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            if (isOpen == false)
+            {
+                isOpen = true;
+                //Scale to smaller
+                await ((Frame)sender).ScaleTo(0.8, 50, Easing.Linear);
+                //Wait a moment
+                await Task.Delay(100);
+                //Scale to normal
+                await ((Frame)sender).ScaleTo(1, 50, Easing.Linear);
+
+                //Show FloatMenuItem1
+                FloatMenuItem1.IsVisible = true;
+                await FloatMenuItem1.TranslateTo(0, 0, 100);
+                await FloatMenuItem1.TranslateTo(0, -20, 100);
+                await FloatMenuItem1.TranslateTo(0, 0, 200);
+
+                //Show FloatMenuItem2
+                FloatMenuItem2.IsVisible = true;
+                await FloatMenuItem2.TranslateTo(0, 0, 100);
+                await FloatMenuItem2.TranslateTo(0, -20, 100);
+                await FloatMenuItem2.TranslateTo(0, 0, 200);
+
+                //Show FloatMenuItem3
+                FloatMenuItem3.IsVisible = true;
+                await FloatMenuItem3.TranslateTo(0, 0, 100);
+                await FloatMenuItem3.TranslateTo(0, -20, 100);
+                await FloatMenuItem3.TranslateTo(0, 0, 200);
+            }
+            else
+            {
+                isOpen = false;
+                //Scale to smaller
+                await ((Frame)sender).ScaleTo(0.8, 50, Easing.Linear);
+                //Wait a moment
+                await Task.Delay(100);
+                //Scale to normal
+                await ((Frame)sender).ScaleTo(1, 50, Easing.Linear);
+
+                //Hide FloatMenuItem1
+                await FloatMenuItem1.TranslateTo(0, 0, 100);
+                await FloatMenuItem1.TranslateTo(0, -20, 100);
+                await FloatMenuItem1.TranslateTo(0, 0, 200);
+                FloatMenuItem1.IsVisible = false;
+
+                //Hide FloatMenuItem2
+                await FloatMenuItem2.TranslateTo(0, 0, 100);
+                await FloatMenuItem2.TranslateTo(0, -20, 100);
+                await FloatMenuItem2.TranslateTo(0, 0, 200);
+                FloatMenuItem2.IsVisible = false;
+
+                //Hide FloatMenuItem3
+                await FloatMenuItem3.TranslateTo(0, 0, 100);
+                await FloatMenuItem3.TranslateTo(0, -20, 100);
+                await FloatMenuItem3.TranslateTo(0, 0, 200);
+                FloatMenuItem3.IsVisible = false;
+            }
+
+        }
+
+        private void FloatMenuItem1Tap_OnTapped(object sender, EventArgs e)
+        {
+            LabelStatus.Text = "Menu 1";
+        }
+
+        private void FloatMenuItem2Tap_OnTapped(object sender, EventArgs e)
+        {
+            LabelStatus.Text = "Menu 3";
+        }
+
+        private void FloatMenuItem3Tap_OnTapped(object sender, EventArgs e)
+        {
+            LabelStatus.Text = "Event Log";
+        }
+
+
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            
+
             await GetUserLocation();
 
             BindingContext = new MapViewModel(Navigation, geoLocate);
@@ -50,14 +128,19 @@ namespace EcoBlocApp_test.Views
         {
             userLocation = new GeolocateUser();
             geoLocate = await userLocation.GetCurrentLocation();
-            
+
             ;
         }
 
-
     }
-
-
-
-
 }
+
+
+
+
+    
+
+
+
+
+
